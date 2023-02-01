6499d8a5d3ea536b7156dd474d75bc07378963f9
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-02-01 15:58:50 +0530

    deleted extra files

--- API/classes.py
+++ API/classes.py
@@ -1,15 +1,18 @@
+# Import the required libraries
 from datetime import date
 from enum import Enum
 
 from pydantic import BaseModel, EmailStr
 
 
+# Declare the category enum
 class CategoryEnum(str, Enum):
     category_1 = "review"
     category_2 = "complaint"
     category_3 = "suggestion"
 
 
+# Declare the review class
 class Review(BaseModel):
     name: str
     review: str
@@ -18,6 +21,7 @@ class Review(BaseModel):
     review_date: date
 
 
+# Declare the order class
 class Order(BaseModel):
     name: str
     email: EmailStr
